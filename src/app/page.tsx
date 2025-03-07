96c49264b97845aced53d232116849e4b739cab4
abdelmouiz <a_youb@estin.dz>
2025-03-07 09:33:24 -1100

    the first veiw inside the structure of the project and create a new form with new Schema for user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,25 @@
-import Link from "next/link";
+"use client"
+import React ,{useState} from 'react'
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
-
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
+function page() {
+  const [username, setUserName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password , setPassword] = useState("")
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
-        </div>
-      </main>
-    </HydrateClient>
-  );
+    <div className='w-screen  h-screen  flex justify-center items-center'>
+      <form action="" className='flex justify-between items-center flex-col gap-5 border-2 rounded-xl  w-1/2 h-1/2 p-5'>
+      <h1>Sign up</h1>
+     <div className='flex flex-col justify-center items-center gap-5 '>
+     <input onChange={(e)=>setUserName(e.target.value)} className='p-2 shadow-xl rounded-xl' type="text" placeholder='Enter The Username ' />
+      <input onChange={(e)=>setEmail(e.target.value)} className='p-2 shadow-xl rounded-xl' type="email" placeholder='Enter The Email ' />
+      <input onChange={(e)=>setPassword(e.target.value)} className='p-2 shadow-xl rounded-xl' type="password " placeholder='Enter The Password  ' />
+     </div>
+     <button type='submit' className='bg-blue-500 text-white pb-2 pt-2 pl-5 pr-5 rounded-md'>
+        Sign Up
+     </button>
+      </form>
+    </div>
+  )
 }
+
+export default page
