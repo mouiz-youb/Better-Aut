c5fea43c31da345d52fd28b63ad35505bd770a8a
abdelmouiz <a_youb@estin.dz>
2025-03-11 10:58:58 -1100

    -------------------

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,35 @@
 "use client"
 import React from 'react'
+import { auth } from '@/Lib/auth'
+import { useRouter } from 'next/navigation'
 import { useLocalStorage } from '@/hook/useLocalStorage '
+import {toast} from "react-hot-toast"
 function page() {
   const {user ,token } = useLocalStorage()
+  const router = useRouter()
+  const signOut =async ()=>{
+    try {
+       const response = await auth.api.signOut({
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      })
+      console.log("Sign-out response:", response);
+      if(response.success === true ){
+        // romove token and user data form the local storage
+        localStorage.removeItem('user')
+        localStorage.removeItem('token')
+        router.push('/signin')
+        
+      toast.success("Signed out successfully!");
+      } else {
+        throw new Error("Sign-out failed");
+      }
+    } catch (error) {
+      console.error("Error during sign-out:", error);
+      toast.error("Failed to sign out. Please try again.");
+    }
+  }
   return (
     <div className='flex justify-center items-center flex-col gap-5 w-screen  '>
       <div className='flex justify-center items-center flex-col gap-5 '>
@@ -15,7 +42,9 @@ function page() {
         <p>Please log in to access the home page.</p>
       )}
       </div>
-      <div className='flex justify-center items-center '></div>
+      <div className='flex justify-center items-center '>
+        <button onClick={signOut}>Sign out</button>
+      </div>
     </div>
   )
 }
